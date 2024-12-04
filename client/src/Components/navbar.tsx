35a68515094e57ed4f5187762c1d7d8c640aebe1
Phoorich Maliwan <s6604062620026@email.kmutnb.ac.th>
2024-12-04 11:47:26 +0700

    new

---
+++ client/src/Components/navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function navbar() {
+  return <div>navbarasdads</div>;
+}
+
+export default navbar;
---
+++ client/src/Components/selct.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function selct() {
+  return <div>selctasdas</div>;
+}
+
+export default selct;
